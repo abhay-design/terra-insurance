6b5e0ee35d150541286fa41456c498f7bc189f22
Abhay <abhay@cleardigital.com>
2025-05-14 17:02:36 +0530

    update static-nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,71 @@ async function Header() {
 
   return (
     <header>
-      <Link href={"/"}> {settings.data.site_title}</Link>
+      {/* <Link href={"/"}> {settings.data.site_title}</Link> */}
+      <div className="top-nav py-[7px!important]">
+        <div className="container">
+          <div className="menu-block relative flex flex-wrap items-center justify-end">
+            <ul className="flex flex-wrap list-none">
+              <li className="relative mr-[10px!important]">
+                <Link
+                  href="#"
+                  className="home text-[#bebebe] text-[10px] font-bold pl-[23px!important]  relative mr-[11px!important]"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="relative mr-[10px!important]">
+                <Link
+                  href="#"
+                  className="tel text-[#bebebe] text-[10px] font-bold pl-[23px!important]  relative mr-[11px!important]"
+                >
+                  Call Us
+                </Link>
+              </li>
+              <li className="relative mr-[10px!important]">
+                <Link
+                  href="#"
+                  className="mail text-[#bebebe] text-[10px] font-bold pl-[23px!important] relative mr-[11px!important]"
+                >
+                  Email
+                </Link>
+              </li>
+              <li className="relative mr-[10px!important]">
+                <Link
+                  href="#"
+                  className="apply text-[#bebebe] text-[10px] font-bold pl-[23px!important] relative mr-[11px!important]"
+                >
+                  Apply Now
+                </Link>
+              </li>
+            </ul>
+            <div className="block-user-login relative">
+              <ul className="list-none">
+                <li className="mr-[10px!important] relative">
+                  <Link
+                    href={"#"}
+                    className="login text-[#bebebe] text-[10px] font-bold pl-[23px!important]  relative mr-[11px!important]"
+                  >
+                    Insured Login
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="block-search-form relative">
+              <form className="w-full relative">
+                <label htmlFor="search-box"></label>
+                <input
+                  type="text"
+                  id="search-box"
+                  placeholder="search"
+                  className="py-[6px!important] pl-[6px!important] pr-[30px!important] text-[12px] bg-[#fff] w-[155px] h-[26px] outline:none"
+                />
+                <span className="btn-search"></span>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <nav>
         <ul>

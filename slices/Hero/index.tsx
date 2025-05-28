9f2b7707762a99f4218511c4a148a8efbd77fbfc
Abhay <abhaydec48@gmail.com>
2025-05-28 19:44:46 +0530

    set hero-banner

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -18,7 +18,7 @@ const Hero: FC<HeroProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="relative min-h-[460px] p-0 mt-[-95px] "
+      className="relative min-h-[460px] p-0 mt-[-95px!important] pb-[70px!important] flex  items-end"
     >
       <div className="bg-img w-full h-full absolute top-0 left-0 z-[-1]">
         <PrismicNextImage
@@ -26,39 +26,41 @@ const Hero: FC<HeroProps> = ({ slice }) => {
           className="w-full h-full bg-no-repeat bg-cover"
         />
       </div>
-      <div className="upper-div bg-[#ffffffd9] px-[20px!important] pt-[18px!important] pb-[14px!important] w-fit ">
-        <PrismicRichText
-          field={slice.primary.heading}
-          components={{
-            heading1: ({ children }) => (
-              <h1 className="text-[32px] font-thin leading-[34px] uppercase text-[#20384c]">
-                {children}
-              </h1>
-            ),
-          }}
-        />
-      </div>
+      <div className="container">
+        <div className="upper-div bg-[#ffffffd9] px-[20px!important] pt-[18px!important] pb-[14px!important] w-fit ">
+          <PrismicRichText
+            field={slice.primary.heading}
+            components={{
+              heading1: ({ children }) => (
+                <h1 className="text-[32px] font-thin leading-[34px] uppercase text-[#20384c]">
+                  {children}
+                </h1>
+              ),
+            }}
+          />
+        </div>
 
-      <div className="lower-div bg-[#00529c] px-[20px!important]  pt-[13px!important] pb-[8px!important] w-fit">
-        <PrismicRichText
-          field={slice.primary.body}
-          components={{
-            paragraph: ({ children }) => (
-              <p className="text-[#fff] m-0 text-[22px] leading-[24px] font-thin">
-                {children}
-              </p>
-            ),
-          }}
-        />
-      </div>
+        <div className="lower-div bg-[#00529c] px-[20px!important]  pt-[13px!important] pb-[8px!important] w-fit">
+          <PrismicRichText
+            field={slice.primary.body}
+            components={{
+              paragraph: ({ children }) => (
+                <p className="text-[#fff] m-0 text-[22px] leading-[24px] font-thin">
+                  {children}
+                </p>
+              ),
+            }}
+          />
+        </div>
 
-      <div className="btn-wrapper mt-[20px!important]  flex flex-wrap ">
-        <PrismicNextLink
-          field={slice.primary.button_link}
-          className="btn-blue text-[13px] pt-[7px!important] pr-[50px!important] pb-[4px!important] pl-[20px!important] text-[#fff] font-thin inline-block relative uppercase  bg-[#00529c]"
-        >
-          {slice.primary.button}
-        </PrismicNextLink>
+        <div className="btn-wrapper mt-[20px!important]  flex flex-wrap ">
+          <PrismicNextLink
+            field={slice.primary.button_link}
+            className="btn-blue text-[13px] pt-[7px!important] pr-[50px!important] pb-[4px!important] pl-[20px!important] text-[#fff] font-thin inline-block relative uppercase  bg-[#00529c]"
+          >
+            {slice.primary.button}
+          </PrismicNextLink>
+        </div>
       </div>
     </section>
   );
